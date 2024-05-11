3826443e730676ccbf7bd639ee2a9246ad244bae
vishalwaykar <vishalwaykar85@gmail.com>
2024-05-11 21:04:57 +0530

    product model created, also added some code improvements in category model.

--- models/ecommerce/category.model.js
+++ models/ecommerce/category.model.js
@@ -4,4 +4,4 @@ const categorySchema = new mongoose.Schema({
 
 }, { timestamps: true } );
 
-export const Category = mongoose.model("Category", categorySchema)
+export const Category = mongoose.model( "Category", categorySchema );
